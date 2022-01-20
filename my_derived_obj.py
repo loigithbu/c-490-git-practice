693b3d6c192b7e47907b49dfa32accd8a2d27750
Loi Ha <loi.ha@yotes.collegeofidaho.edu>
2022-01-20 14:18:46 -0700

    error

--- my_derived_obj.py
+++ my_derived_obj.py
@@ -2,7 +2,7 @@ import my_object
 
 class MyDerivedObj(my_object.MyObject):
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
     def __str__(self):
         return self.greeting
